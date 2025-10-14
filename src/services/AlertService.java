0c0ccc159a170e2b9d48240362a753327329d1ef
Sai Abhiram <saiabhiram0902@gmail.com>
2025-10-14 19:44:16 +0530

    File Handling & Data Storage

--- src/services/AlertService.java
+++ src/services/AlertService.java
@@ -2,13 +2,28 @@ package services;
 
 import models.Tourist;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class AlertService {
+    private void writeToLog(String message) {
+        try (FileWriter fw = new FileWriter("alerts_log.txt", true)) {
+            fw.write(LocalDateTime.now() + " " + message + "\n");
+        } catch (IOException e) {
+            System.out.println("Error writing to log file: " + e.getMessage());
+        }
+    }
+
     public void panicAlert(Tourist t) {
-        System.out.println("[ALERT] Panic button pressed by: " + t.getName());
-        System.out.println("Location: " + t.getLatitude() + ", " + t.getLongitude());
+        String msg = "[PANIC] " + t.getName() + " at " + t.getLatitude() + ", " + t.getLongitude();
+        System.out.println(msg);
+        writeToLog(msg);
     }
 
     public void restrictedZoneAlert(Tourist t) {
-        System.out.println("[ALERT] " + t.getName() + " entered a restricted zone!");
+        String msg = "[RESTRICTED] " + t.getName() + " entered restricted zone!";
+        System.out.println(msg);
+        writeToLog(msg);
     }
 }
